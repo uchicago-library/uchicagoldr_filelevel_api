2ed12254672611c0d8eb7f8c02f059f49c236157
Brian Balsamo <balsamo@uchicago.edu>
2016-07-14 16:52:55 -0500

    Setting up 1 layer of presform exploration - presform materialsuites being arbitrarily nestable and not having an opaque identifier may cause some problems.

--- ldr_staging_api.py
+++ ldr_staging_api.py
@@ -21,8 +21,10 @@ _STAGING_ENV_PATH = Config.staging_env_path
 def escape(s):
     return quote(s, safe='')
 
+
 def unescape(s):
     return unquote(s)
+
 
 def get_stages_list():
     return [x.name for x in scandir(_STAGING_ENV_PATH) if x.is_dir()]
@@ -158,7 +160,30 @@ class MaterialSuite(Resource):
 class Presforms(Resource):
     def get(self, stage_id, segment_id, ms_id):
         # Return a representation of all available presforms
-        pass
+        try:
+            try:
+                s = get_stage(stage_id)
+            except:
+                raise ValueError("Bad Stage Identifier")
+            segment = [x for x in s.segment_list if x.identifier == segment_id]
+            if len(segment) != 1:
+                raise ValueError("Bad Segment Identifier")
+            segment = segment[0]
+            print(ms_id)
+            ms = [x for x in segment.get_materialsuite_list() if escape(x.content.item_name) == escape(ms_id)]
+            if len(ms) != 1:
+                raise ValueError("Bad MaterialSuite Identifier")
+            ms = ms[0]
+            r = {}
+            r['stage_id'] = s.identifier
+            r['segment_id'] = segment.identifier
+            r['materialsuite_id'] = ms_id
+            r['presforms'] = [escape(x.content.item_name) for x in ms.presform_list]
+            return jsonify(
+                APIResponse("success", data=r).dictify()
+            )
+        except Exception as e:
+            return jsonify(APIResponse("fail", errors=[str(type(e)) + ": " + str(e)]).dictify())
 
     def post(self, stage_id, segment_id, ms_id):
         # Add a presform
@@ -172,10 +197,103 @@ class Presforms(Resource):
 class Presform(Resource):
     def get(self, stage_id, segment_id, ms_id, presform_id):
         # Return the presform file
-        pass
+        try:
+            try:
+                s = get_stage(stage_id)
+            except:
+                raise ValueError("Bad Stage Identifier")
+            segment = [x for x in s.segment_list if x.identifier == segment_id]
+            if len(segment) != 1:
+                raise ValueError("Bad Segment Identifier")
+            segment = segment[0]
+            print(ms_id)
+            ms = [x for x in segment.get_materialsuite_list() if escape(x.content.item_name) == escape(ms_id)]
+            if len(ms) != 1:
+                raise ValueError("Bad MaterialSuite Identifier")
+            ms = ms[0]
+            presform = [x for x in ms.presform_list if x.content.item_name == presform_id]
+            if len(presform) != 1:
+                raise ValueError("Bad Presform Identifier")
+            presform = presform[0]
+            r = {}
+            r['stage_id'] = s.identifier
+            r['segment_id'] = segment.identifier
+            r['materialsuite_id'] = ms_id
+            r['presform_id'] = presform_id
+            r['techmd'] = [x.item_name for x in presform.technicalmetadata_list]
+            r['premis'] = None
+            if presform.premis:
+                r['premis'] = presform.premis.item_name
+            r['presforms'] = []
+            if presform.presform_list:
+                r['presforms'] = [x.item_name for x in presform.presform_list]
+            return jsonify(
+                APIResponse("success", data=r).dictify()
+            )
+        except Exception as e:
+            return jsonify(APIResponse("fail", errors=[str(type(e)) + ": " + str(e)]).dictify())
+
 
     def delete(self, stage_id, segment_id, ms_id, presform_id):
         # Delete this presform
+        pass
+
+
+class PresformContent(Resource):
+    def get(self, stage_id, segment_id, ms_id, presform_id):
+        try:
+            try:
+                s = get_stage(stage_id)
+            except:
+                raise ValueError("Bad Stage Identifier")
+            segment = [x for x in s.segment_list if x.identifier == segment_id]
+            if len(segment) != 1:
+                raise ValueError("Bad Segment Identifier")
+            segment = segment[0]
+            print(ms_id)
+            ms = [x for x in segment.get_materialsuite_list() if escape(x.content.item_name) == escape(ms_id)]
+            if len(ms) != 1:
+                raise ValueError("Bad MaterialSuite Identifier")
+            ms = ms[0]
+            presform = [x for x in ms.presform_list if x.content.item_name == presform_id]
+            if len(presform) != 1:
+                raise ValueError("Bad Presform Identifier")
+            presform = presform[0]
+            presform_content = presform.content
+            return send_file(presform_content.open(), as_attachment=True, attachment_filename=presform_content.item_name)
+        except Exception as e:
+            return jsonify(APIResponse("fail", errors=[str(type(e)) + ": " + str(e)]).dictify())
+
+    def delete(self, stage_id, segment_id, ms_id, presform_id):
+        pass
+
+
+class PresformPremis(Resource):
+    def get(self, stage_id, segment_id, ms_id, presform_id):
+        try:
+            try:
+                s = get_stage(stage_id)
+            except:
+                raise ValueError("Bad Stage Identifier")
+            segment = [x for x in s.segment_list if x.identifier == segment_id]
+            if len(segment) != 1:
+                raise ValueError("Bad Segment Identifier")
+            segment = segment[0]
+            print(ms_id)
+            ms = [x for x in segment.get_materialsuite_list() if escape(x.content.item_name) == escape(ms_id)]
+            if len(ms) != 1:
+                raise ValueError("Bad MaterialSuite Identifier")
+            ms = ms[0]
+            presform = [x for x in ms.presform_list if x.content.item_name == presform_id]
+            if len(presform) != 1:
+                raise ValueError("Bad Presform Identifier")
+            presform = presform[0]
+            presform_premis = presform.premis
+            return send_file(presform_premis.open(), as_attachment=True, attachment_filename=presform_premis.item_name)
+        except Exception as e:
+            return jsonify(APIResponse("fail", errors=[str(type(e)) + ": " + str(e)]).dictify())
+
+    def delete(self, stage_id, segment_id, ms_id, presform_id):
         pass
 
 
@@ -200,7 +318,7 @@ class Techmds(Resource):
             r['stage_id'] = s.identifier
             r['segment_id'] = segment.identifier
             r['materialsuite_id'] = ms_id
-            r['tech_mds'] = [x.item_name for x in ms.technicalmetadata_list]
+            r['tech_mds'] = [escape(x.item_name) for x in ms.technicalmetadata_list]
             return jsonify(
                 APIResponse("success", data=r).dictify()
             )
@@ -233,7 +351,7 @@ class Techmd(Resource):
             if len(ms) != 1:
                 raise ValueError("Bad MaterialSuite Identifier")
             ms = ms[0]
-            techmd = [x for x in ms.technicalmetadata_list if x.item_name == techmd_id]
+            techmd = [x for x in ms.technicalmetadata_list if escape(x.item_name) == escape(techmd_id)]
             if len(techmd) != 1:
                 raise ValueError("Bad Technical Metadata Identifier")
             techmd = techmd[0]
@@ -249,6 +367,9 @@ class Techmd(Resource):
 class Content(Resource):
     def get(self, stage_id, segment_id, ms_id):
         try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('body', type=bool)
+            args = parser.parse_args()
             try:
                 s = get_stage(stage_id)
             except:
@@ -319,8 +440,10 @@ api.add_resource(Stage, '/stages/<string:stage_id>')
 api.add_resource(Segment, '/stages/<string:stage_id>/<string:segment_id>')
 api.add_resource(MaterialSuite, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>')
 api.add_resource(Presforms, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/presforms')
-api.add_resource(Presform, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/presforms/<string:presform_id>')
+api.add_resource(Presform, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/presforms/<path:presform_id>')
+api.add_resource(PresformContent, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/presforms/<path:presform_id>/content')
+api.add_resource(PresformPremis, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/presforms/<path:presform_id>/premis')
 api.add_resource(Techmds, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/techmds')
-api.add_resource(Techmd, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/techmds/<string:techmd_id>')
+api.add_resource(Techmd, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/techmds/<path:techmd_id>')
 api.add_resource(Content, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/content')
 api.add_resource(Premis, '/stages/<string:stage_id>/<string:segment_id>/<path:ms_id>/premis')
